db85cfe78a96e05c6dfd90cda1bbe49c5017d9ee
sunnypanjwani7 <sunnypanjwani7@gmail.com>
2023-09-02 15:29:00 +0530

    Pub
    Pubish

---
+++ src/components/data.js
@@ -0,0 +1,82 @@
+const data = [
+  {
+    id: 1,
+    question: "कल्प सूत्र में कितने कल्प ों का वर्णन किया गया है?",
+    answers: [
+      {
+        option: "A",
+        text: "10",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "8",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "20",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "24",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "सम्यक के बाद भगवान महावीर ने कितने मुख्य जन्म लिए हैं?",
+    answers: [
+      {
+        option: "A",
+        text: "26",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "27",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "25",
+        correct: true,
+      },
+      {
+        option: "D",
+        text: "24",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "भगवान महावीर के कितने गण और गंधार थे?",
+    answers: [
+      {
+        option: "A",
+        text: "9,11",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "11,11",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "84,84",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "8,8",
+        correct: false,
+      },
+    ],
+  }
+];
+
+export default data;
